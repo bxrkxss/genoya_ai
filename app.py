71ce3b347addb7e270b7be2b388d8c99799c99ba
Borkiss <102585380+bxrkxss@users.noreply.github.com>
2023-12-28 21:06:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
-import requests
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+import httpx
 
-# Замените 'YOUR_API_KEY' на ваш фактический API ключ
 API_KEY = 'AIzaSyB-WcIhGE44RKrrDxrepTMf3vB-BiAzCko'
+TELEGRAM_BOT_TOKEN = '6905096531:AAHBnKWydimQzAeYdc1l4Ha1m3CsZqQ104Y'
 
-# URL для запроса к API Google
-url = 'https://us-central1-aiplatform.googleapis.com/v1/projects'
+async def start(update, context):
+    await update.message.reply_text('Привет! Напиши мне что-нибудь, и я использую API Google.')
 
-# Параметры для запроса
-payload = {
-    'key': API_KEY
-}
+async def help_command(update, context):
+    await update.message.reply_text('Отправьте мне текст, и я обработаю его с помощью API Google.')
 
-# Отправка запроса
-response = requests.get(url, params=payload)
+async def handle_message(update, context):
+    user_message = update.message.text
+    url = 'https://us-central1-aiplatform.googleapis.com/v1/predictions'
+    payload = {'key': API_KEY, 'q': user_message}
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, params=payload)
+    if response.status_code == 200:
+        await update.message.reply_text(response.json())
+    else:
+        await update.message.reply_text('Произошла ошибка при обработке вашего сообщения.')
 
-# Обработка ответа от API Google
-if response.status_code == 200:
-    # API-ключ рабочий
-    print('API-ключ рабочий')
-else:
-    # API-ключ не рабочий
-    print('API-ключ не рабочий')
+application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
+application.add_handler(CommandHandler("start", start))
+application.add_handler(CommandHandler("help", help_command))
+application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+application.run_polling()
